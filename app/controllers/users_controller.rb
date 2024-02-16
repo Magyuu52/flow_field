4a89c32feb4ffba55d56e9b47b28d56c3d57f697
Magyuu52 <sota.n509852@gmail.com>
2024-02-16 18:00:34 +0900

    メールアドレスとパスワードを記入してログインできるようにアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,4 +34,14 @@ class UsersController < ApplicationController
 
   def login_form
   end
+
+  def login
+    @user =User.find_by(email: params[:email],password: params[:password]) 
+    if @user 
+      flash[:notice] = "ログインに成功しました"
+      redirect_to ('/')
+    else
+      render :login_form
+    end
+  end
 end
