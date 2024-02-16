c16b6effc055f22277f95499cd865a3b18fdf585
Magyuu52 <sota.n509852@gmail.com>
2024-02-16 17:57:56 +0900

    userのルーティングに、ログイン機能とログアウト機能を使うためのルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
 
   get resources :users, except: [:index, :destroy]
+  get 'login' => 'users#login_form'
+  post 'login' => 'users#login'
+  post 'logout' => 'users#logout'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
