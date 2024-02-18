6eaa459e168cd564964f6ac12d5f302f53dd92fa
Magyuu52 <sota.n509852@gmail.com>
2024-02-18 20:41:57 +0900

    userコントローラーのカラムにスペルミスがあったため修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update(params.permit(:name, :introduction, :password, :password_confim, :image))
+    if @user.update(params.permit(:name, :introduction, :password, :password_confirm, :image))
       flash[:notice] = "アカウント情報を更新しました"
       redirect_to action: :show
     else
