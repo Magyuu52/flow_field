238605a0db160168893983b7ec532a73199f89e2
Magyuu52 <sota.n509852@gmail.com>
2024-02-20 07:07:17 +0900

    ゲストユーザーの新規登録または保存ができた時、ログイン状態を保持してトップページへ移動するように記載

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -3,5 +3,8 @@ class GuestSessionsController < ApplicationController
     user = User.find_or_create_by(name: "ゲストユーザー", email: "guest@example.com") do |user|
       user.password = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
     end
+    session[:user_id] = user.id
+    flash[:notice] = "ゲストユーザーとしてログインしました"
+    redirect_to root_path
   end
 end
