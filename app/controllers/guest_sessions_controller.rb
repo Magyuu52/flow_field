831e9065deaf36721ddceae4bd31e410bd13f313
Magyuu52 <sota.n509852@gmail.com>
2024-02-20 07:02:30 +0900

    ゲストユーザーのパスワードを作成する際に、SecureRandom.alphanumericメソッドでランダムに英字を生成し、パスワードの最後に必ず英字+数字の２文字が入るようにして英字のみor数字のみのパスワードが生成されないように記載

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,6 +1,7 @@
 class GuestSessionsController < ApplicationController
   def create
     user = User.find_or_create_by(name: "ゲストユーザー", email: "guest@example.com") do |user|
+      user.password = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
     end
   end
 end
