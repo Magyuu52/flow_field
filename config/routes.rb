da4e5cdc77fb7400721924211d388259e23ec590
Magyuu52 <sota.n509852@gmail.com>
2024-02-20 06:48:57 +0900

    ゲストログインするためのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'login' => 'users#login_form'
   post 'login' => 'users#login'
   post 'logout' => 'users#logout'
+
+  post 'guest_login' => 'guest_sessions#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
