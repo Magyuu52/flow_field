0d1693ebee48f730c14b2de3696be41437971b47
Magyuu52 <sota.n509852@gmail.com>
2024-02-21 19:40:20 +0900

    非ログイン状態の時に、特定のアクションを無効化するためのアクションを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
   def set_current_user    
     @current_user = User.find_by(id: session[:user_id])  
   end
+
+  def authenticate_user
+  end
 end
