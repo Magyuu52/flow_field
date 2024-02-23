610c9d966bd5a5843ffc9662a067ba0329f90853
Magyuu52 <sota.n509852@gmail.com>
2024-02-23 14:57:43 +0900

    メールアドレスのバリデーションの正規表現の標準ライブラリが適用されるように記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,12}+\z/i
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: "は半角6~12文字英大文字・小文字・数字それぞれ1文字以上含む必要があります"}
   has_one_attached :image
 end
