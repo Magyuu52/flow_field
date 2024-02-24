f03d1d930a2f5454b32a70215d7c0208f357355e
Magyuu52 <sota.n509852@gmail.com>
2024-02-24 14:33:00 +0900

    Postsコントローラーに、resoucesメソッドで設定したルーティングのアクションを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
   def index
     @posts = Posts.where(user_id: @current_user)
   end
+  
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
