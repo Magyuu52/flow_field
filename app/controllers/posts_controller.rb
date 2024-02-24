43b43799d7a37c1281f42dca1e84cd1eccaa3fad
Magyuu52 <sota.n509852@gmail.com>
2024-02-24 14:56:27 +0900

    新規投稿の保存に成功した時、投稿 一覧ページへリダイレクトし、失敗すると投稿画面に戻るように記載

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,12 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(params.require(:post).permit(:title, :spot_name, :content, :spot_image, flow_video))
+    if @post.save
+      flash[:notice] = "新規投稿の作成に成功しました"
+      redirect_to :posts_index
+    else
+      render "new"
+    end
   end
 
   def show
