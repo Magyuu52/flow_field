99d797016f0ec9b296a7e24cce6a887261e8b803
Magyuu52 <sota.n509852@gmail.com>
2024-02-25 15:22:05 +0900

    ログイン状態の場合に制限されるアクションによって、制限が不要なアクションに影響があったため、制限アクションを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, {only: [:edit, :update]}
-  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :forbid_login_user, {only: [:new, :login_form, :login]}
   before_action :ensure_correct_user, {only: [:edit, :update]}
 
   def new
