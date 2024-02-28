00b8f4a00394febf5476669f971421d0da78686f
Magyuu52 <sota.n509852@gmail.com>
2024-02-28 18:11:49 +0900

    Postモデルに住所登録または変更した時に、geocoderが緯度・経度のデータを登録・更新するように記載

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   has_one_attached :spot_image
   has_one_attached :flow_video
+  geocoded_by :address
+  after_validation :geocode
 
   def user
     return User.find_by(id: self.user_id)
