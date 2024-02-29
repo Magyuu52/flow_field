1378135e34cbdf67d14b35ebed32a14145effc4a
Magyuu52 <sota.n509852@gmail.com>
2024-02-29 20:03:34 +0900

    likeコントローラーのcreateアクションに、いいねをした時に保存され、その投稿の詳細ページへリダイレクトするように定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,7 @@
 class LikesController < ApplicationController
   def create
-    
+    @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
+    @like.save
+    redirect_to post_path(params[:post_id]) 
   end
 end
