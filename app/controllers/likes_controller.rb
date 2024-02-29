dc0f0cccc4b450fde90a2d7734801ba091270173
Magyuu52 <sota.n509852@gmail.com>
2024-02-29 20:08:16 +0900

    likeコントローラーのdestroyアクシ ョンに、いいねを取り消した時に保存されたいいねの記録を削除して、その投稿の詳細ページへリダイレクトするように定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,6 +6,8 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    
+    @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
+    @like.destroy
+    redirect_to post_path(params[:post_id])
   end
 end
