bcdb8041a0d2507899bc60ab56ef3d1249b93b5a
Magyuu52 <sota.n509852@gmail.com>
2024-03-01 20:14:48 +0900

    Postモデルに、ユーザー及びいいねモデルとのアソシエーションを記載

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_one_attached :flow_video
   geocoded_by :address
   after_validation :geocode
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   def user
     return User.find_by(id: self.user_id)
