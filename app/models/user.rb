adea7d2886a218246803a5f20a0bfe55b09c29f4
Magyuu52 <sota.n509852@gmail.com>
2024-03-03 21:28:14 +0900

    userモデルに、フォロー・フォロワーの一覧画面でフォロワーを表示させるために、throughで経由するテーブル、sourceで参照するカラムを指定しつつアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   def posts
     return Post.where(user_id: self.id)
