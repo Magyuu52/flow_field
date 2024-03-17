8449f5ab8e248be3fe9ce66b2ea06924aeb9db6d
Magyuu52 <sota.n509852@gmail.com>
2024-03-17 18:57:34 +0900

    likesコントローラーのcreate・destroyアクションの処理が完了した時に、そのページ上で更新されるようにrequest.refererメソッドへ書き換え

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
     @like.save
-    redirect_to post_path(params[:post_id]) 
+    redirect_to request.referer 
   end
 
   def destroy
     @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
     @like.destroy
-    redirect_to post_path(params[:post_id])
+    redirect_to request.referer
   end
 end
