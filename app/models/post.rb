577c9dbbba95ae5380882e4f0f6e95056dd1550b
Magyuu52 <sota.n509852@gmail.com>
2024-03-21 07:22:34 +0900

    postモデルに、投稿をいいね順に並べ替えるために必要となる、特定の投稿といいねしたユーザーとのアソシエーションを定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,7 @@ class Post < ApplicationRecord
   after_validation :geocode
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 
   def user
     return User.find_by(id: self.user_id)
