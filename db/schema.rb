502c34f68cdc9c1a551d7880bba5f05c2ea6f561
Magyuu52 <sota.n509852@gmail.com>
2024-03-24 11:01:32 +0900

    rails db:migrateを実行してデータベースの変更を反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_22_021554) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_24_015924) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,7 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_22_021554) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
-    t.string "user_name"
     t.string "price"
   end
 
