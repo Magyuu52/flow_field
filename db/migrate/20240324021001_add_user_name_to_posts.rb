2077e2efa3d5b524858cee1c32a7f4c96cc4a1c0
Magyuu52 <sota.n509852@gmail.com>
2024-03-24 11:18:52 +0900

    rails db:migrateを実行してデータ ベースの変更を反映(sqlでのエラーを回避するために、マイグレーションファイルの記載を削除)

--- db/migrate/20240324021001_add_user_name_to_posts.rb
+++ db/migrate/20240324021001_add_user_name_to_posts.rb
@@ -1,5 +1,4 @@
 class AddUserNameToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_column :posts, :user_name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_24_015924) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_24_021001) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_24_015924) do
     t.float "latitude"
     t.float "longitude"
     t.string "price"
+    t.string "user_name"
   end
 
   create_table "relationships", force: :cascade do |t|
