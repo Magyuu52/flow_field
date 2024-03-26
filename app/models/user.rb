a1c147e29fde83b8ed8ea81cbb1841b876496dcd
Magyuu52 <sota.n509852@gmail.com>
2024-03-26 19:56:04 +0900

    userモデルのコードの記載のずれを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   EMAIL_REGEXP = /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,12}+\z/i
 
-  validates :name, {presence: true, length: { minimum: 1, maximum: 20 }}
-  validates :introduction,  length: { maximum: 100 }
+  validates :name, { presence: true, length: { minimum: 1, maximum: 20 } }
+  validates :introduction, length: { maximum: 100 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
-  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: "は半角6~12文字英大文字・小文字・数字それぞれ1文字以上含む必要があります"}
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: "は半角6~12文字英大文字・小文字・数字それぞれ1文字以上含む必要があります" }
   has_one_attached :image
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
