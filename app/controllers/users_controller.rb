3767002b3b5f1f89b7aec0165ad2f1f609c71237
Magyuu52 <sota.n509852@gmail.com>
2024-03-27 09:06:40 +0900

    userコントローラーのloginアクションのコードにおいて、インデントのずれがあったため修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
   end
 
   def login
-    @user =User.find_by(email: params[:email],password: params[:password]) 
+    @user = User.find_by(email: params[:email], password: params[:password]) 
     if @user
       session[:user_id] = @user.id
       flash[:notice] = "ログインに成功しました"
