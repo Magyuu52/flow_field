66d62dc1e6fcca32d15207887bfde6a942e47dc1
Magyuu52 <sota.n509852@gmail.com>
2024-04-01 03:46:48 +0900

    rails db:migrateを実行して、データベースに変更内容を反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_24_021001) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_31_184230) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_24_021001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "experience"
+    t.string "password_confirm"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
