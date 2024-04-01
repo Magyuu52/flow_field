2e1037443df2a7c0f85cf476eb4cb6caf615981c
Magyuu52 <sota.n509852@gmail.com>
2024-04-02 06:39:05 +0900

    userモデルに、確認用パスワードのためのカスタムバリデーションを適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 100 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: "は半角6~12文字英大文字・小文字・数字それぞれ1文字以上含む必要があります" }
+  validate :check_password
   has_one_attached :image
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
