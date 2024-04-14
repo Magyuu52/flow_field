ef0c04e2955ade1b25bb4503076217452adc8c5f
Magyuu52 <sota.n509852@gmail.com>
2024-04-15 06:37:34 +0900

    ユーザーの 新規登録に成功するテストに、新規登録ページで必須項目を入力して新規登録ボタンを押すと、ログインに成功してトップページへ移り、ログイン成功を知らせるフラッシュメッセージが表示される結果が返ってくるように記載

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe "Users", type: :system do
   describe 'ユーザー新規登録' do
     context '入力情報が正しい場合' do
       it 'ユーザーの新規登録に成功すること' do
+        visit new_user_path
+        fill_in '名前', with: 'rails-testuser'
+        fill_in 'メールアドレス', with: 'rails@example.com'
+        fill_in 'パスワード', with: 'Railstest1'
+        fill_in '確認用パスワード', with: 'Railstest1'
+        click_on '新規登録をする'
+        expect(current_path).to eq root_path
+        expect(page).to have_content 'ユーザーの新規登録に成功しました'
       end
     end
 
