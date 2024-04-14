1338316be25dc30c4cfd2882116cffc1007958c2
Magyuu52 <sota.n509852@gmail.com>
2024-04-15 06:33:25 +0900

    入力情報が正しい場合、ユーザーの新規登録に成功するテストを記載

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "Users", type: :system do
   describe 'ユーザー新規登録' do
     context '入力情報が正しい場合' do
+      it 'ユーザーの新規登録に成功すること' do
+      end
     end
 
     context '入力情報に誤りがある場合' do
