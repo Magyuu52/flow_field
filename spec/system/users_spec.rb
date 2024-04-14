e8cb52a5923e0f09d79a52a770ca200dc70ef0d2
Magyuu52 <sota.n509852@gmail.com>
2024-04-15 06:28:37 +0900

    ユーザーの新規登録についてのテスト項目の中に、入力情報が正しい場合と入力情報に誤りがある場合でテスト内容を分けるために、contextを使って場合分け

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   describe 'ユーザー新規登録' do
+    context '入力情報が正しい場合' do
+    end
+
+    context '入力情報に誤りがある場合' do
+    end
   end
 end
