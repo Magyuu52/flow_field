a7fd9fd9beb16124cabb75b4c7ed431ddef3669b
Magyuu52 <sota.n509852@gmail.com>
2024-04-15 00:00:44 +0900

    ログインに関するテストの項目の中に、入力されるユーザー情報の正誤で場合分けするためにcontextを記載

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -4,5 +4,10 @@ RSpec.describe "Sessions", type: :system do
   let(:user) { create(:user) }
 
   describe 'ログイン' do
+    context 'ユーザーの入力情報が正しい場合' do
+    end
+
+    context 'ユーザーの入力情報に誤りがある場合' do
+    end
   end
 end
