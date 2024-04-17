a7a57422e4541afb9bed844985c75040137cb8b3
Magyuu52 <sota.n509852@gmail.com>
2024-04-18 06:02:35 +0900

    posts_spec.rbに、テストを実行するために必要となるテストデータを作成するように定義

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :system do
+  let(:user) { create(:user) }
+  let(:post) { create(:post, user: user) }
+  let(:other_post) { create(:post) }
 end
