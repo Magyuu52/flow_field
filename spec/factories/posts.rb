ee8d71dcbf7e6a956c3147ffac3cd4d71fa1c04f
Magyuu52 <sota.n509852@gmail.com>
2024-04-18 05:58:26 +0900

    postのテストデータ作成ファイルに、active storageを使って用意した画像が保存されるように設定

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     address { '大阪府大阪市港区海岸通１丁目１' }
     content { Faker::Lorem.word }
     user
+
+    after(:build) do |post|
+      post.spot_image.attach(io: File.open("spec/fixtures/image/test_post.jpg"), filename: "test_post.jpg")
+    end
   end
 end
