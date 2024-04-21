0851d51dfa4b289fcafcfd1c813c36e747e68581
Magyuu52 <sota.n509852@gmail.com>
2024-04-21 21:49:17 +0900

    ユーザーの自己紹介文が100文字を超える場合は無効であることのテストのテストデータの自己紹介文100文字を超えておらず、テストが通らなかったため修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe "User", type: :model do
     end
 
     it 'ユーザーの自己紹介文が100文字を超える場合は無効であること' do
-      user_over_letter_introduction = FactoryBot.build(:user, introduction: 'a' * 100)
+      user_over_letter_introduction = FactoryBot.build(:user, introduction: 'a' * 101)
       expect(user_over_letter_introduction).to be_invalid
     end
   end
