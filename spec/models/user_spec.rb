1811f0901a976a7715a95afb79675ca8bad31446
Magyuu52 <sota.n509852@gmail.com>
2024-04-21 13:45:58 +0900

    必須項目の名前とメールアドレスと パスワードがある場合は有効であることのテストに、factorybotを使って有効なユーザーのテストデータを作成し、そのデータがユーザーのモデルとして有効な値である結果が返ってくるように記載

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "User", type: :model do
   describe 'ユーザーのバリデーション関連' do
     it '必須項目の名前とメールアドレスとパスワードがある場合は有効であること' do
+      valid_user = FactoryBot.build(:user)
+      expect(valid_user).to be_valid
     end
   end
 end
