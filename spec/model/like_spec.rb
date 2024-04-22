3086136137e49a1e4d2dba1d441cfb1060cd698d
Magyuu52 <sota.n509852@gmail.com>
2024-04-22 17:47:15 +0900

    データの保存機能に関するテスト項目に、likeモデルに必要なパラメーターが揃っていれば有効であることのテストを記載

--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe Like, type: :model do
     let(:user) { create(:user) }
     let(:post) { create(:post, user: user) }
     let(:like) { create(:like, post_id: post.id, user_id: user.id) }
+
+    it 'likeモデルに必要なパラメーターが揃っていれば有効であること' do
+    end
   end
 end
