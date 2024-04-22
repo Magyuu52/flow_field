2dad7d63373ba9cbe18fe39e3f85b5eb084cb51a
Magyuu52 <sota.n509852@gmail.com>
2024-04-22 16:14:36 +0900

    データの保存機能のテスト項目に、relationモデルに必要なパラメーターが揃っていれば有効であることのテストを記載

--- spec/model/relationship_spec.rb
+++ spec/model/relationship_spec.rb
@@ -3,5 +3,8 @@ require 'rails_helper'
 RSpec.describe Relationship, type: :model do
   describe 'データの保存機能' do
     let(:relationship) { create(:relationship) }
+    
+    it "relationモデルに必要なパラメーターが揃っていれば有効であること" do
+    end
   end
 end
