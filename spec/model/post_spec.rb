5b5540a026cce278a2ef8624da137b554e894d4d
Magyuu52 <sota.n509852@gmail.com>
2024-04-22 10:58:01 +0900

    必須項目のタイトルと住所がある場合は有効であることのテストに、factorybotを使って有効な投稿のテストデータを作成し、そのデータは投稿モデルとして有効である結果が返ってくるように記載

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "Post", type: :model do
   describe '投稿のバリデーション関連' do
     it '必須項目のタイトルと住所がある場合は有効であること' do
+      valid_post = FactoryBot.create(:post)
+      expect(valid_post).to be_valid
     end
   end
 end
