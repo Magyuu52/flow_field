9bb5152e862feedc3c511a4dd53028dd45cad4ba
Magyuu52 <sota.n509852@gmail.com>
2024-04-22 16:18:42 +0900

    relationモデルに必要なパラメーターが揃っていれば有効であることのテストに、全てのパラメーターが揃っている事前に作成したrelationモデルのテストデータは、relationモデルのデータとして有効である結果が返ってくるように記載

--- spec/model/relationship_spec.rb
+++ spec/model/relationship_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Relationship, type: :model do
   describe 'データの保存機能' do
     let(:relationship) { create(:relationship) }
     
-    it "relationモデルに必要なパラメーターが揃っていれば有効であること" do
+    it 'relationモデルに必要なパラメーターが揃っていれば有効であること' do
+      expect(relationship).to be_valid
     end
   end
 end
