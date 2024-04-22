b885e51ef0fdaf111ca5efc510a7560f05d114c1
Magyuu52 <sota.n509852@gmail.com>
2024-04-22 17:46:36 +0900

    データの保存機能に関するテスト項目に、テスト実行時に必要となるテストデータを事前に作成するように定義

--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -2,5 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   describe 'データの保存機能' do
+    let(:user) { create(:user) }
+    let(:post) { create(:post, user: user) }
+    let(:like) { create(:like, post_id: post.id, user_id: user.id) }
   end
 end
