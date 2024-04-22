1b9071d4084ecc7b4a51cb915de335766d302e7a
Magyuu52 <sota.n509852@gmail.com>
2024-04-22 18:19:09 +0900

    relationship_spec.rbのデータの保存機能のテスト項目の各テスト名を、適切なテスト名に変更

--- spec/model/relationship_spec.rb
+++ spec/model/relationship_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe Relationship, type: :model do
   describe 'データの保存機能' do
     let(:relationship) { create(:relationship) }
     
-    it 'relationモデルに必要なパラメーターが揃っていれば有効であること' do
+    it 'relationshipモデルに必要なパラメーターが揃っていれば有効であること' do
       expect(relationship).to be_valid
     end
 
-    it 'relationモデルのfollower_idがnilの場合は無効であること' do
+    it 'relationshipモデルのfollower_idがnilの場合は無効であること' do
       relationship.follower_id = nil
       expect(relationship).to be_invalid
     end
 
-    it 'relationモデルのfollowed_idがnilの場合は無効であること' do
+    it 'relationshipモデルのfollowed_idがnilの場合は無効であること' do
       relationship.followed_id = nil
       expect(relationship).to be_invalid
     end
